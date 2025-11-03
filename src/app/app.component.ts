ec48bcaaea1f3fa15e83fbdd586fd094d3545369
Oded Bartov <odedoded777@gmail.com>
2025-11-03 13:31:05 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,13 @@ import { trigger, style, animate, transition } from '@angular/animations';
   ]
 })
 export class AppComponent implements OnInit {
-  data: any[] = [{value: 30, label: 'aaa', color: 'red'}
-  //, {value: 30, label: 'bbb', color: 'blue'}//, {value: 40, label: 'ccc', color: 'green'}
-];
+  data: any[] = [{ value: 30, label: 'aaa', color: 'red' }
+    //, {value: 30, label: 'bbb', color: 'blue'}//, {value: 40, label: 'ccc', color: 'green'}
+  ];
   total: number = 0;
   startAngles: number[] = [];
   slices: any[] = [];
-  
+
   currentIndex = 0;
   animationState: string | null = null;
   chartOptions = {}
@@ -35,17 +35,16 @@ export class AppComponent implements OnInit {
   loading = false;
   carouselItems: any[] = [];
   skills = [{ name: "C#", level: 5 },
-  { name: "Angular", level: 4 },
+  { name: "Angular", level: 5 },
   { name: "Java", level: 3 },
   { name: "Node", level: 2 },
+  { name: "kafka", level: 3 },
   { name: "WPF", level: 3 },
   { name: "EF", level: 4 },
   { name: "SQL", level: 3 },
-  { name: "GCP", level: 4 },
-  { name: "AWS", level: 1 },
-  { name: "Azure", level: 1 },
-  { name: "GitHub", level: 4 },
-  { name: "TFS", level: 4 }
+  { name: "GCP", level: 3 },
+  { name: "AWS", level: 4 },
+  { name: "Azure", level: 2 }
   ]
 
   ngOnInit() {
@@ -60,7 +59,8 @@ export class AppComponent implements OnInit {
       currentAngle += percentage;
       this.slices.push({
         label: this.data[i].label,
-        color: this.data[i].color});
+        color: this.data[i].color
+      });
     }
   }
 
@@ -73,9 +73,8 @@ export class AppComponent implements OnInit {
   getConicGradient(index: number): string {
     const startAngle = this.startAngles[index];
     const endAngle = startAngle + (this.data[index].value / this.total) * 360;
-    return `conic-gradient(from ${startAngle}deg to ${endAngle}deg, ${
-      this.slices[index].color
-    } 0%, ${this.slices[index].color} 100%)`;
+    return `conic-gradient(from ${startAngle}deg to ${endAngle}deg, ${this.slices[index].color
+      } 0%, ${this.slices[index].color} 100%)`;
   }
 
   sendWhatsapp() {
@@ -121,30 +120,35 @@ export class AppComponent implements OnInit {
   }
 
   initCarousel() {
-    this.carouselItems = [{
-      company: "ONE", title: "Team leader", dateFrom: "03/2023", dateTo: "Present", responsibilities: [
-        "Directed the comprehensive design and specification of the entire system while overseeing programming team schedules. Took full responsibility for task management, ensuring timely and successful completion",
-        "Exercised strategic decision-making authority in shaping code architecture, design, and efficiency, with a focus on guiding the development team. Provided valuable assistance in navigating intricate tasks, leveraging a deep understanding of system intricacies and industry best practices to optimize overall project outcomes"]
-    },
-    {
-      company: "Capitolis", title: "Back-end developer", dateFrom: "08/2023", dateTo: "12/2023", responsibilities: [
-        "Solely managed the maintenance and development of an expansive system, serving as the backbone for some of the largest banks and hedge funds globally",
-        "Played a key role in proactively addressing system features, promptly resolving bugs, and maintaining the overall integrity of the server infrastructure"]
-    },
-    {
-      company: "mPrest (iron dome)", title: "Full-stack developer", dateFrom: "07/2021", dateTo: "07/2022", responsibilities: [
-        "Shouldered the responsibility for the maintenance and expansion of a critical system dedicated to identifying and intercepting airborne threats. Executed key initiatives to ensure the ongoing reliability, scalability, and advanced capabilities of the system, contributing to enhanced security measures",
-        "Directed the control of a large-scale desktop application by meticulously addressing end-to-end bugs and conducting comprehensive analysis of features. Implemented strategic solutions to ensure seamless functionality and enhance overall performance, contributing to the optimization of the desktop application at a large scale",
-        "Engineered and implemented a sophisticated communication platform for cameras and sensors, enabling seamless detection of objects and threats in the sky. Instrumental in building a robust system to enhance communication and detection capabilities for improved security measures"]
-    },
-    {
-      company: "Sela group", title: "Full stack developer + lecturer", dateFrom: "08/2019", dateTo: "07/2021", responsibilities: [
-        "Implementing new features in the GUI, creating new functionality and maintain corresponding UI testing on the desktop application",
-        "Expanded and enhanced the server infrastructure, integrating essential APIs to facilitate the seamless provision of required data and capture precise inputs. Played a key role in fortifying the server's capabilities, ensuring it effectively met the demands of the system by optimizing data access and input processes",
-        "Teaching one-on-one and full-classes lessons, practicing frond and back end, servers, clouds, data bases and more",
-        "Built an android app using angular nativescript, implementing google maps SDK and using firebase for server, DB and storage. The app is called Tripper and is free on google play"
-      ]
-    }]
+    this.carouselItems = [
+      {
+        company: "InfraEdge", title: "Full stack developer", dateFrom: "03/2024", dateTo: "Present", responsibilities: ["Collaborated with cross-functional teams to build and optimize large-scale AWS-deployed applications, implementing new features and fixing critical bugs across both client and server",
+          "Research and find new solutions to new problems, together with keeping the strict effiency policy the systems already have"]
+      },
+      {
+        company: "ONE", title: "Team leader", dateFrom: "03/2023", dateTo: "03/2024", responsibilities: [
+          "Directed the comprehensive design and specification of the entire system while overseeing programming team schedules. Took full responsibility for task management, ensuring timely and successful completion",
+          "Exercised strategic decision-making authority in shaping code architecture, design, and efficiency, with a focus on guiding the development team. Provided valuable assistance in navigating intricate tasks, leveraging a deep understanding of system intricacies and industry best practices to optimize overall project outcomes"]
+      },
+      {
+        company: "Capitolis", title: "Back-end developer", dateFrom: "08/2023", dateTo: "12/2023", responsibilities: [
+          "Solely managed the maintenance and development of an expansive system, serving as the backbone for some of the largest banks and hedge funds globally",
+          "Played a key role in proactively addressing system features, promptly resolving bugs, and maintaining the overall integrity of the server infrastructure"]
+      },
+      {
+        company: "mPrest (iron dome)", title: "Full-stack developer", dateFrom: "07/2021", dateTo: "07/2022", responsibilities: [
+          "Shouldered the responsibility for the maintenance and expansion of a critical system dedicated to identifying and intercepting airborne threats. Executed key initiatives to ensure the ongoing reliability, scalability, and advanced capabilities of the system, contributing to enhanced security measures",
+          "Directed the control of a large-scale desktop application by meticulously addressing end-to-end bugs and conducting comprehensive analysis of features. Implemented strategic solutions to ensure seamless functionality and enhance overall performance, contributing to the optimization of the desktop application at a large scale",
+          "Engineered and implemented a sophisticated communication platform for cameras and sensors, enabling seamless detection of objects and threats in the sky. Instrumental in building a robust system to enhance communication and detection capabilities for improved security measures"]
+      },
+      {
+        company: "Sela group", title: "Full stack developer + lecturer", dateFrom: "08/2019", dateTo: "07/2021", responsibilities: [
+          "Implementing new features in the GUI, creating new functionality and maintain corresponding UI testing on the desktop application",
+          "Expanded and enhanced the server infrastructure, integrating essential APIs to facilitate the seamless provision of required data and capture precise inputs. Played a key role in fortifying the server's capabilities, ensuring it effectively met the demands of the system by optimizing data access and input processes",
+          "Teaching one-on-one and full-classes lessons, practicing frond and back end, servers, clouds, data bases and more",
+          "Built an android app using angular nativescript, implementing google maps SDK and using firebase for server, DB and storage. The app is called Tripper and is free on google play"
+        ]
+      }]
   }
 
   prev() {
